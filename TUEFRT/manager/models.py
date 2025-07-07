b9b8c6c1b5e7799b6674cfe323734b543ac923c6
Aadit Chadda <aaditkobe268@gmail.com>
2025-07-07 01:56:51 -0400

    setting up models and the django admin panel

--- TUEFRT/manager/models.py
+++ TUEFRT/manager/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Responder(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=10, null=True)
+    email = models.CharField(max_length=250, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Inventory(models.Model):
+    product_id = models.CharField(max_length=75)
+    product_name = models.CharField(max_length=200)
+    quantity = models.IntegerField(null=True)
+    description = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Delivered', 'Delivered'),
+        ('Re-stocked', 'Re-stocked'),
+    )
+    supplier = models.CharField(max_length=200)
+    datetime = models.DateTimeField(auto_now_add=True, null=True)
+    #product =
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
