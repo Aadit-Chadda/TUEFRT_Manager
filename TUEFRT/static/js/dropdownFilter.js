ffb8d194b39ef53bdb2f08158c81146b93d970df
Tony Tran <tpt.tran7@gmail.com>
2025-08-09 17:06:23 -0400

    Fixed searchFilter and dropdownFilter inconsistency in inventory

---
+++ TUEFRT/static/js/dropdownFilter.js
@@ -0,0 +1,39 @@
+const filter = document.getElementById('searchFilter');
+const items = document.querySelectorAll('.product'); // oughta change that name up soon lol also querySelectorAll places it in a list.
+const searchFilter = document.getElementById('search-bar');
+var globalSelectedType = 'All'; // by default it's 'All' so there's no null errors.
+// need to set item to be .product and not prdocut-left
+
+filter.addEventListener('change', function() {
+    const selectedType = this.value;
+    globalSelectedType = selectedType;
+
+    items.forEach(item => {
+        const itemType = item.getAttribute('data-type');
+        
+        // Only want item to be searchable if the itemType matches the selectedType.
+        if (selectedType === 'All' || itemType === selectedType){ // They stay weird for some reason.
+            item.style.display = 'flex'; 
+        } else {
+            item.style.display = 'none';
+        }
+    })
+});
+
+searchFilter.addEventListener('input', function(event) {
+    const searchTerm = event.target.value.toLowerCase();
+    //const itemDiv = document.querySelectorAll('.product');
+
+    items.forEach(item => { 
+        const itemText = item.textContent.toLowerCase();
+        const itemType = item.getAttribute('data-type');
+
+        if(itemText.includes(searchTerm) && itemType === globalSelectedType) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+
+
